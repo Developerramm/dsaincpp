dc27a63e37cb487dafe64c258a1a752bcb8fb2c5
Ram kumar <rr697144@gmail.com>
2026-07-16 13:30:08 +0530

    cpp basic

---
+++ basic/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 5;
+    cout<< "Value of a is " << a << endl;
+
+    cout<< &a << endl;
+
+    // cout << "Namaste Bharat" << endl;
+
+    // int n;
+    // cout <<"Enter a number : ";
+    // cin >> n;
+    // cout <<"Your number is " << n << endl;
+
+    return 0;
+}
