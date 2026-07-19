0ae7b1ea7d486b84fb31c6c0cbb79e3bc572db41
Ram kumar <rr697144@gmail.com>
2026-07-19 16:30:39 +0530

    student marks and grade

---
+++ function/main4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+char getGrade(char marks)
+{
+
+    if (marks >= 90)
+    {
+        return 'A';
+    }
+    else if (marks >= 80)
+    {
+        return 'B';
+    }
+    else if (marks >= 70)
+    {
+        return 'C';
+    }
+    else if (marks >= 60)
+    {
+        return 'D';
+    }
+    else
+    {
+        return 'E';
+    }
+}
+
+int main()
+{
+
+    int marks;
+    cout << "Enter the marks : ";
+    cin >> marks;
+
+    char finalGrade = getGrade(marks);
+    cout << finalGrade << endl;
+
+    return 0;
+}
