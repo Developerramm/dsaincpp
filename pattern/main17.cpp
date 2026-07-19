cb4c071152fe5e04ae9ff598167cf825f5e39360
Ram kumar <rr697144@gmail.com>
2026-07-19 14:34:59 +0530

    hollow full pyramid

---
+++ pattern/main17.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+
+    for (int row = 0; row < n; row++)
+    {
+
+        for (int space = 0; space < n - row - 1; space++)
+        {
+
+            cout << " ";
+        }
+
+        if (row == 0 || row == n - 1)
+        {
+            for (int col = 0; col < row + 1; col++)
+            {
+                cout << "* ";
+            }
+        }
+        else
+        {
+            for (int col = 0; col < row + 1; col++)
+            {
+                if (col == 0 || col == row)
+                {
+                    cout << "* ";
+                }
+                else
+                {
+                    cout << "  ";
+                }
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
