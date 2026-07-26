1357a8799b8f33c00919af54ced8b0eab3d194c5
Ram kumar <rr697144@gmail.com>
2026-07-26 10:16:20 +0530

    numeric palindromic equilateral pyramid

--- pattern/main20.cpp
+++ pattern/main20.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n;
     cout << "Enter a number : ";
     cin >> n;
 
-    for(int row = 0; row <n; row++){
+    for (int row = 0; row < n; row++)
+    {
 
-        
+        for (int space = 0; space < n - row - 1; space++)
+        {
+            cout << "  ";
+        }
+
+        int count;
+        for (count = 0; count < row + 1; count++)
+        {
+            cout << count + 1 << " ";
+        }
+
+        for (int count2 = count - 1; count2 > 0; count2--)
+        {
+            cout << count2 << " ";
+        }
+
+        cout << endl;
     }
-
 
     return 0;
 }
