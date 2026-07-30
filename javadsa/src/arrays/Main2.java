db36bf35495fbe119f10edf415404954b4117248
Ram kumar <rr697144@gmail.com>
2026-07-30 12:01:25 +0530

    move all negative to the left side of array

---
+++ javadsa/src/arrays/Main2.java
@@ -0,0 +1,74 @@
+package arrays;
+
+public class Main2 {
+
+    // move negative to the left side of array
+    public static void moveNegative(int[] arr) {
+
+        int[] temp = new int[arr.length];
+        int count = 0;
+
+        // move all positive element to the temp array
+        for (int x : arr) {
+            if (x > 0) {
+                temp[count++] = x;
+            }
+        }
+
+        // move all negative element to the temp array
+        for (int x : arr) {
+            if (x < 0) {
+                temp[count++] = x;
+            }
+        }
+
+        // move element to the temp array to original array
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = temp[i];
+        }
+
+    }
+
+    // move negative to left side of array
+    public static void moveNegative1(int[] arr) {
+        int negativecount = 0;
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] < 0) {
+                negativecount++;
+
+                int temp = arr[i];
+                int j;
+                for (j = i; j < arr.length - negativecount; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                arr[j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("move all negative number to the left side of array");
+
+        int[] arr = { 1, -1, 3, 2, -7, -5, 11, 6 };
+
+        // print array
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+
+        // moveNegative(arr);
+        moveNegative1(arr);
+
+        System.out.println();
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+
+        System.out.println();
+
+        System.out.println(arr);
+    }
+
+}
