132cf49e385c5ee659fb063572c66e0a40b4f787
ram kumar <rr697144@gmail.com>
2026-08-03 08:03:13 +0530

    create string

---
+++ string/main6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+
+    // create string 
+    string str;
+
+    // cin >> str;
+    // take input in string
+    getline(cin, str);
+
+    // print string 
+    cout << str << endl;
+
+    return 0;
+}
